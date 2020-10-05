fa271fc2d5bfef5747f3bd7d207a17b8d0bb0d41
TheOnionJR <sander.loken@gmail.com>
2020-10-05 11:23:33 +0200

    Added multithreading for massive speed boost :)

--- stego_img_sort/sort_istego100k.py
+++ stego_img_sort/sort_istego100k.py
@@ -1,13 +1,29 @@
-import json, os
+import json, os, time
 from shutil import copyfile
+from multiprocessing import Pool
 
 json_path = "img_properties.json"
 src_path = "E:\\ds-1\\stego\\"
 dst_path = "E:\\ds-4\\"
 
-with open(json_path, 'r', encoding='utf-8', errors='ignore') as the_file:
-    params = json.load(the_file)
-    for image in params:
-        src_img = src_path + image
-        dst_img = str(dst_path) + str(params[image]["steg_algorithm"]) + "\\" + str(image)
-        copyfile(src_img,dst_img)
+
+def img_cpy(list):
+    global src_path
+    global dst_path
+    src_img = src_path + list[0]
+    dst_img = str(dst_path) + str(list[1]) + "\\" + str(list[0])
+    copyfile(src_img,dst_img)
+
+t = time.time()
+
+if __name__ == '__main__':
+    p = Pool(8)
+    with open(json_path, 'r', encoding='utf-8', errors='ignore') as the_file:
+        params = json.load(the_file)
+        images = []
+        for image in params:
+            images.append((image,params[image]["steg_algorithm"]))
+        print(images)
+        p.map(img_cpy, images)
+    elapsed = time.time() - t
+    print("Total time: " + str(elapsed))
